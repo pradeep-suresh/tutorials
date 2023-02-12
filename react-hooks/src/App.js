d605c64b56008ebbfda6605df0c1f089bd911c49
Pradeep Suresh <pradeepsuresh@Pradeeps-MacBook-Pro-2.local>
2023-02-11 20:36:30 -0800

    UseReducer Hook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,10 +1,12 @@
 //import PostsView from '../src/feature/posts/postsView'
 import PostsView from './PostsView'
+import CounterOneView from './feature/counter/CounterOneView'
+import CounterTwoView from './feature/counter/CounterTwoView'
 
 function App() {
   return (
     <div>
-      <PostsView />
+      <CounterTwoView />
     </div>
   )
 }
